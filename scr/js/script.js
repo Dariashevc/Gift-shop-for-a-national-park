f8e3b21635d5e4d8861e28016e5672d773ad2895
Dariashevc <151487077+Dariashevc@users.noreply.github.com>
2025-03-31 02:55:03 -0600

    commit

--- scr/js/script.js
+++ scr/js/script.js
@@ -1,20 +1,14 @@
-// Toggle the sidebar to show and hide filter options and toggle arrow direction
+// Toggle the sidebar to show and hide filter options
 function toggleFilter() {
     var filterOptions = document.getElementById("filter-options");
     var sideNav = document.getElementById("side-nav");
-    var arrow = document.getElementById("toggle-arrow");
-    
     // Toggle the display of the filter options
     if (filterOptions.style.display === "none") {
         filterOptions.style.display = "block";
         sideNav.style.width = "220px"; // Show full sidebar
-        arrow.classList.remove("fa-chevron-down");
-        arrow.classList.add("fa-chevron-up"); // Change arrow direction
     } else {
         filterOptions.style.display = "none";
         sideNav.style.width = "80px"; // Collapse sidebar to just show Filters
-        arrow.classList.remove("fa-chevron-up");
-        arrow.classList.add("fa-chevron-down"); // Change arrow direction
     }
 }
 
