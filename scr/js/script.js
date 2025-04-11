67e2c7a4d4fe0c257261e732faf0e1947fd17bb5
Dariashevc <dashashev2008@gmail.com>
2025-04-11 14:06:32 -0600

    c

--- scr/js/script.js
+++ scr/js/script.js
@@ -1,14 +1,12 @@
-// Function to toggle the filter options display
 function toggleFilter() {
     var filterOptions = document.getElementById("filter-options");
     var sideNav = document.getElementById("side-nav");
-    // Toggle the display of the filter options
     if (filterOptions.style.display === "none") {
         filterOptions.style.display = "block";
-        sideNav.style.width = "220px"; // Show full sidebar
+        sideNav.style.width = "220px"; 
     } else {
         filterOptions.style.display = "none";
-        sideNav.style.width = "80px"; // Collapse sidebar to just show Filters
+        sideNav.style.width = "80px";
     }
 }
 
@@ -17,6 +15,4 @@ function updatePrice() {
     var price = document.getElementById("price-range").value;
     document.getElementById("price-display").textContent = "$" + price;
 }
-
-// Set initial state to collapsed (only Filters visible)
-document.getElementById("filter-options").style.display = "none"; // Hide filter options initially
+document.getElementById("filter-options").style.display = "none"; 
